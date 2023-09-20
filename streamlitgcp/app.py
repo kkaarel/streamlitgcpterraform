9f783e9ba5e9407260fcb35c638850b9644111f7
kkaarel <informations@hotmail.com>
2023-09-20 19:51:05 +0300

    streamlit

---
+++ streamlitgcp/app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from streamlit.web.server.websocket_headers import _get_websocket_headers
+
+st.set_page_config(
+    page_title="Debugger",
+    page_icon="👋",
+)
+
+st.write("# Welcome to the Streamlit Google clodu! 👋")
+
+st.sidebar.success("Select a demo above.")
+
+
+headers = _get_websocket_headers()
+
+st.write(headers) # have a look at what else is in the dict
+
+
+st.markdown(
+    """
+
+"""
+)
